3847fa2b01c0e131c0106dacb5a9d1069ebab090
lim-yeonkyu <kevin000606@handong.ac.kr>
2023-09-14 09:40:43 +0900

    First commit

---
+++ src/test/java/edu/handong/happymanback/HappyManBackApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.handong.happymanback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyManBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
